30de36e8e38a8b10064af9a0f29b3a2920247e05
potato1543 <kaila9511@gmail.com>
2017-07-21 15:48:50 -0700

    AsyncLoader

--- app/src/main/java/com/example/owner/newsapp/MainActivity.java
+++ app/src/main/java/com/example/owner/newsapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.owner.newsapp;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,12 +24,16 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements GithubAdapter.ItemClickListener {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList>, GithubAdapter.ItemClickListener {
     static final String TAG = "MainActivity";
     private EditText search;
     private ProgressBar progress;
     private RecyclerView rv;
     private GithubAdapter adapter;
+
+    //uniquely identify the loader
+    private static final int NEWS_LOADER = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,19 @@ public class MainActivity extends AppCompatActivity implements GithubAdapter.Ite
 
         rv = (RecyclerView) findViewById(R.id.recyclerview_news);
         rv.setLayoutManager(new LinearLayoutManager(this));
+
+        // this called the loading manager
+        loadThem();
+        // intializes all the values in the asyncloader
+        getSupportLoaderManager().initLoader(NEWS_LOADER, null, this);
+
+    }
+
+    // this methods loads the data if not all ready loaded
+    private void loadThem() {
+        LoaderManager loaderManager = getSupportLoaderManager();
+        loaderManager.restartLoader(NEWS_LOADER, null, this).forceLoad();
+        adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -57,8 +76,8 @@ public class MainActivity extends AppCompatActivity implements GithubAdapter.Ite
 
         if (itemNumber == R.id.search) {
             String s = search.getText().toString();
-            NetworkTask task = new NetworkTask(s);
-            task.execute();
+            loadThem();
+
         }
         return true;
 
@@ -73,51 +92,62 @@ public class MainActivity extends AppCompatActivity implements GithubAdapter.Ite
         }
     }
 
-    class NetworkTask extends AsyncTask<URL, Void, ArrayList<NewsItem>> {
+    // AsyncTaskLoader
+    // Loader is the same as Task it just runs the program better
 
-        String query;
+    @Override
+    public Loader<ArrayList> onCreateLoader(int id, final Bundle args) {
 
-        NetworkTask(String s) {
-            query = s;
-        }
+        return new AsyncTaskLoader<ArrayList>(this) {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progress.setVisibility(View.VISIBLE);
-
-        }
-
-
-        @Override
-        protected ArrayList<NewsItem> doInBackground(URL... params) {
-            ArrayList<NewsItem> result = null;
-            //  URL url = NetworkUtils.buildUrl(query, "stars");
-            URL url = NetworkUtils.buildUrl("the-next-web", "latest", "bf84b88bcd544f8094c1083036bffaae");
-            Log.d(TAG, "Starturl: " + url.toString());
-            try {
-                String json = NetworkUtils.getResponseFromHttpUrl(url);
-                result = NetworkUtils.parseJSON(json);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(final ArrayList<NewsItem> data) {
-            super.onPostExecute(data);
-            progress.setVisibility(View.GONE);
-            if (data != null) {
-               rv.setAdapter(adapter);
-                adapter.setData(data);
-            } else {
-
-                Log.d(TAG, "no data");
+            // same as Preexecute will check the args for info
+            @Override
+            protected void onStartLoading() {
+                if (args != null) {
+                    Log.d(TAG, "no new info ");
+                }
+                super.onStartLoading();
+                progress.setVisibility(View.VISIBLE);
             }
 
+            // run the json in the background and make sure every matches before it is displays in PostExecute
+            @Override
+            public ArrayList loadInBackground() {
+                ArrayList<NewsItem> result = null;
+                //  URL url = NetworkUtils.buildUrl(query, "stars");
+                URL url = NetworkUtils.buildUrl("the-next-web", "latest", "bf84b88bcd544f8094c1083036bffaae");
+                Log.d(TAG, "Starturl: " + url.toString());
+                try {
+                    String json = NetworkUtils.getResponseFromHttpUrl(url);
+                    result = NetworkUtils.parseJSON(json);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return result;
+            }
+        };
+    }
+
+
+    // OnLoadFinished works the same way as postExecute did. Displays the json
+    @Override
+    public void onLoadFinished(Loader<ArrayList> loader,ArrayList data) {
+        progress.setVisibility(View.GONE);
+        if (data != null) {
+            rv.setAdapter(adapter);
+            adapter.setData(data);
+        } else {
+
+            Log.d(TAG, "no data");
         }
     }
+
+    @Override
+    public void onLoaderReset(Loader<ArrayList> loader) {
+
+    }
+
 }
